47eeb1802b405139312fadc216aeec5844fe1765
wang-q <wang-q@outlook.com>
2016-06-20 02:34:39 +0800

    Finish command connect

--- lib/App/Rangeops/Command/connect.pm
+++ lib/App/Rangeops/Command/connect.pm
@@ -11,7 +11,12 @@ use constant abstract => 'connect bilaterial links into multilateral ones';
 sub opt_spec {
     return (
         [ "outfile|o=s", "Output filename. [stdout] for screen." ],
-        [ "verbose|v",   "Verbose mode.", ],
+        [   "ratio|r=f",
+            "Break links if length identities less than this ratio, default is [0.9].",
+            { default => 0.9 }
+        ],
+        [ "numeric|n", "Sort chromosome names numerically.", ],
+        [ "verbose|v", "Verbose mode.", ],
     );
 }
 
@@ -105,7 +110,8 @@ sub execute {
             # add edge
             if ( !$graph->has_edge( $range_0, $range_1 ) ) {
                 $graph->add_edge( $range_0, $range_1 );
-                $graph->set_edge_attribute( $range_0, $range_1, "strand", $hit_strand );
+                $graph->set_edge_attribute( $range_0, $range_1, "strand",
+                    $hit_strand );
 
                 print STDERR join "\t", @parts, "\n" if $opt->{verbose};
                 printf STDERR "Nodes %d \t Edges %d\n",
@@ -121,7 +127,7 @@ sub execute {
     }
 
     #----------------------------#
-    # Create cc
+    # Create cc and sort
     #----------------------------#
     my @lines;
     for my $cc ( $graph->connected_components ) {
@@ -130,17 +136,117 @@ sub execute {
 
         next if $copy == 1;
 
-        print STDERR "Copy number of this cc is $copy\n" if $opt->{verbose};
-
         my $line = join "\t", @ranges;
         push @lines, $line;
     }
 
+    my @sorted_lines
+        = @{ App::Rangeops::Common::sort_links( \@lines, $opt->{numeric} ) };
+
     #----------------------------#
-    # Sort
+    # Change strands of ranges based on first range in cc
     #----------------------------#
-    my @sorted_lines = @{ App::Rangeops::Common::sort_links( \@lines, $info_of,
-            $opt->{numeric} ) };
+    my $new_graph = Graph->new( directed => 0 );
+    for my $line (@sorted_lines) {
+        my @ranges = split /\t/, $line;
+        my $copy = scalar @ranges;
+        print STDERR "Copy number of this cc is $copy\n" if $opt->{verbose};
+
+        # transform range to info
+        # set strand to undef
+        # create intspan
+        my @infos = map {
+            my $info = App::RL::Common::decode_header($_);
+            $info->{strand}  = undef;
+            $info->{intspan} = AlignDB::IntSpan->new;
+            $info->{intspan}->add_pair( $info->{start}, $info->{end} );
+            $info;
+        } @ranges;
+
+        # set first node to positive strand
+        $infos[0]->{strand} = "+";
+
+        my $assigned  = AlignDB::IntSpan->new(0);
+        my $unhandled = AlignDB::IntSpan->new;
+        $unhandled->add_pair( 0, $copy - 1 );
+        $unhandled->remove($assigned);
+
+        my @edges;    # store edge pairs. not all nodes connected
+        while ( $assigned->size < $copy ) {
+            for my $i ( $assigned->elements ) {
+                for my $j ( $unhandled->elements ) {
+                    next if !$graph->has_edge( $ranges[$i], $ranges[$j] );
+                    next
+                        if !$graph->has_edge_attribute( $ranges[$i],
+                        $ranges[$j], "strand" );
+                    next if !defined $infos[$i]->{strand};
+
+                    # assign strands
+                    my $hit_strand
+                        = $graph->get_edge_attribute( $ranges[$i], $ranges[$j],
+                        "strand" );
+                    if ( $hit_strand eq "-" ) {
+                        printf " " x 4 . "change strand of %s\n", $ranges[$j]
+                            if $opt->{verbose};
+                        if ( $infos[$i]->{strand} eq "+" ) {
+                            $infos[$j]->{strand} = "-";
+                        }
+                        else {
+                            $infos[$j]->{strand} = "+";
+                        }
+                    }
+                    else {
+                        $infos[$j]->{strand} = $infos[$i]->{strand};
+                    }
+                    $unhandled->remove($j);
+                    $assigned->add($j);
+
+                    # break bad links
+                    my $size_i = $infos[$i]->{intspan}->size;
+                    my $size_j = $infos[$j]->{intspan}->size;
+                    my ( $size_min, $size_max )
+                        = List::MoreUtils::PP::minmax( $size_i, $size_j );
+                    my $diff_ratio = sprintf "%.3f", $size_min / $size_max;
+
+                    if ( $diff_ratio < $opt->{ratio} ) {
+                        printf " " x 4 . "Break links between %s %s\n",
+                            $ranges[$i], $ranges[$j];
+                        printf " " x 4 . "Ratio[%s]\tMin [%s]\tMax[%s]\n",
+                            $diff_ratio, $size_min, $size_max;
+                    }
+                    else {
+                        push @edges, [ $i, $j ];
+                    }
+                }
+            }
+        }
+
+        # transform array_ref back to string
+        my @nodes_new = map { App::RL::Common::encode_header( $_, 1 ) } @infos;
+
+        for my $edge (@edges) {
+            $new_graph->add_edge( $nodes_new[ $edge->[0] ],
+                $nodes_new[ $edge->[1] ] );
+        }
+    }
+
+    #----------------------------#
+    # Recreate cc
+    #----------------------------#
+    my @new_lines;
+    for my $cc ( $new_graph->connected_components ) {
+        my @ranges = @{$cc};
+        my $copy   = scalar @ranges;
+
+        next if $copy == 1;
+
+        my $line = join "\t", @ranges;
+        push @new_lines, $line;
+    }
+
+    my @new_sorted_lines
+        = @{ App::Rangeops::Common::sort_links( \@new_lines, $opt->{numeric} )
+        };
 
     #----------------------------#
     # Output
@@ -153,7 +259,7 @@ sub execute {
         open $out_fh, ">", $opt->{outfile};
     }
 
-    print {$out_fh} "$_\n" for @sorted_lines;
+    print {$out_fh} "$_\n" for @new_sorted_lines;
 
     close $out_fh;
 }
