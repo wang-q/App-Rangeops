6d85aea084f17fecf400e504ad9ec27bdf70c441
wang-q <wang-q@outlook.com>
2016-06-21 06:23:27 +0800

    Add TODOs

--- lib/App/Rangeops.pm
+++ lib/App/Rangeops.pm
@@ -6,6 +6,13 @@ use strict;
 use warnings;
 use App::Cmd::Setup -app;
 
+# TODO: nest (java)
+#   remove locations fully contained by others. egas/blastn_genome.pl
+# TODO: bundle links
+# TODO: filter
+#   * difference of lengthes. egas/proc_cc_chop.pl
+#   * copy numbers.
+
 1;
 
 __END__
